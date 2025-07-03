5743cc98768a07099b39fbb69b51a85616b022bb
JuniorLifenya <Danyelfrilan345@outlook.com>
2025-07-03 18:15:51 +0200

    FINALLY quasi distribution function plottet

---
+++ PHASE_SPACE_stuff/PLOT_wignerFUNC_combo.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt("PHASE_SPACE_stuff/wigner_data.csv", delimiter=",", )
+x, p, W = data[:,0], data[:,1], data[:,2]
+
+fig= plt.figure(figsize=(8,8))
+ax=fig.add_subplot(111,projection="3d")
+plt.tricontourf(x, p, W, 100, cmap='RdBu_r')
+plt.colorbar()
+plt.xlabel("x")
+plt.ylabel("p")
+plt.title("Wigner Function")
+plt.show()
